c51bcb41bb8255d020e1684ea4d664206c40f4b6
Samuel Mauldin <sammauldin01@gmail.com>
2022-11-06 21:01:23 -0600

    Added in Player Turn Actions
    Player can now attack, defend and charge their attack for the next turn.

--- Assets/Scripts/StateMachine/PlayerTurnGameState.cs
+++ Assets/Scripts/StateMachine/PlayerTurnGameState.cs
@@ -7,7 +7,13 @@ public class PlayerTurnGameState : GameState
 {
     [SerializeField] Text _playerTurnTextUI = null;
 
-    int _playerTurnCount = 0;
+    [SerializeField] Health _enemy;
+
+    private int _damage = 10;
+    private bool _charged = false;
+    private int _playerTurnCount = 0;
+
+    public bool _defend = false;
 
     public override void Enter()
     {
@@ -18,6 +24,21 @@ public class PlayerTurnGameState : GameState
         _playerTurnTextUI.text = "Player Turn: " + _playerTurnCount.ToString();
         //hook into events 
         StateMachine.Input.PressedConfirm += OnPressedConfirm;
+    }
+
+    public override void Tick()
+    {
+        base.Tick();
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            BasicAttack();
+        }
+        if (Input.GetKeyDown(KeyCode.X)){
+            Defend();
+        }
+        if (Input.GetKeyDown(KeyCode.Z)){
+            ChargeAttack();
+        }
     }
 
     public override void Exit()
@@ -31,5 +52,39 @@ public class PlayerTurnGameState : GameState
     void OnPressedConfirm()
     {
         StateMachine.ChangeState<EnemyTurnGameState>();
+    }
+
+    private void BasicAttack()
+    {
+        Debug.Log("EnemyAttacked");
+        _enemy.TakeDamage(_damage);
+        if (_charged)
+        {
+            _damage = 10;
+            _charged = false;
+        }
+        StateMachine.ChangeState<EnemyTurnGameState>();
+    }
+
+    private void Defend()
+    {
+        Debug.Log("Defend");
+        _defend = true;
+        StateMachine.ChangeState<EnemyTurnGameState>();
+    }
+
+    private void ChargeAttack()
+    {
+        if (!_charged)
+        {
+            Debug.Log("Charged");
+            _damage = _damage * 2;
+            _charged = true;
+            StateMachine.ChangeState<EnemyTurnGameState>();
+        }
+        else
+        {
+            Debug.Log("You are already charged");
+        }
     }
 }
